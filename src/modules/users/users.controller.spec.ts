c44e0aaa11753cb4e0fa51bd5e4d1c178f1c98e4
ipedraza <ingprogramar2@arrivesafe.co>
2025-03-02 20:58:08 -0500

    preubas unitarias

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,18 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './users.controller';
+import { UserServices } from './users.service';
+import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
+import { RolesGuard } from '../../guards/role-check.guard';
+import { User } from './user.entity';
 
-describe('UsersController', () => {
+describe('UserController', () => {
   let controller: UserController;
+  let userService: UserServices;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-    }).compile();
+      providers: [
+        {
+          provide: UserServices,
+          useValue: {
+            getAllUsers: jest.fn(),
+            getUserById: jest.fn(),
+            createUser: jest.fn(),
+            updateUserById: jest.fn(),
+            deleteUser: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: () => true })
+      .overrideGuard(RolesGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserServices>(UserServices);
   });
 
-  it('should be defined', () => {
+  it('debería estar definido', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('getAllUsers', () => {
+    it('Retorna una lista de usuarios', async () => {
+      const mockUsers: User[] = [
+        {
+          id: 1,
+          name: 'Usuario 1',
+          email: 'usuario1@example.com',
+          created_at: new Date(),
+          // Otras propiedades...
+        },
+        {
+          id: 2,
+          name: 'Usuario 2',
+          email: 'usuario2@example.com',
+          created_at: new Date()
+        },
+      ];
+      jest.spyOn(userService, 'getAllUsers').mockResolvedValue(mockUsers);
+
+      const result = await controller.getAllUsers();
+      expect(result).toEqual(mockUsers);
+    });
+  });
+
 });
--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,18 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserServices } from './users.service';
+import { UserRepository } from './users.repository';
+import { UtilityService } from '../../shared/utility/utility.service';
+import { User } from './user.entity';
 
 describe('UserServices', () => {
   let service: UserServices;
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserServices],
+      providers: [
+        UserServices,
+        {
+          provide: UserRepository,
+          useValue: {
+            getAllUsers: jest.fn(),
+            getUserById: jest.fn(),
+            findByEmail: jest.fn(),
+            createUser: jest.fn(),
+            updateUser: jest.fn(),
+            deleteUser: jest.fn(),
+          },
+        },
+        UtilityService,
+      ],
     }).compile();
 
     service = module.get<UserServices>(UserServices);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
-  it('should be defined', () => {
+  it('debería estar definido', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getAllUsers', () => {
+    it('debería retornar una lista de usuarios', async () => {
+      const mockUsers: User[] = [
+        {
+          id: 1,
+          name: 'Usuario 1',
+          email: 'usuario1@example.com',
+          created_at: new Date(),
+          // Otras propiedades...
+        },
+        {
+          id: 2,
+          name: 'Usuario 2',
+          email: 'usuario2@example.com',
+          created_at: new Date()
+        },
+      ];
+      jest.spyOn(userRepository, 'getAllUsers').mockResolvedValue(mockUsers);
+
+      const result = await service.getAllUsers();
+      expect(result).toEqual(mockUsers);
+    });
+
+    it('Excepción si no hay usuarios', async () => {
+      jest.spyOn(userRepository, 'getAllUsers').mockResolvedValue([]);
+
+      await expect(service.getAllUsers()).rejects.toThrow(
+        'No se encontraron usuarios',
+      );
+    });
+  });
+
+  
 });
