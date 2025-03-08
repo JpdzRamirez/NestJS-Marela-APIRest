ebc7cf94ee3cbc1b223e3efbb812f88faf739bf6
jpdzsoftware <jeremyivanpedraza@gmail.com>
2025-03-08 11:50:00 -0500

    correctivo, dto validación de campos

--- src/modules/type_client/dto/post-AllTypeClient.dto.ts
+++ src/modules/type_client/dto/post-AllTypeClient.dto.ts
@@ -1,6 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString, Matches, IsBoolean, IsOptional, IsNumber, IsInt } from 'class-validator';
-export class PostAllTypeClientDto {
+import { Type } from 'class-transformer';
+import { IsArray, ValidateNested, IsNotEmpty, IsString, Matches, IsInt } from 'class-validator';
 
+export class PostAllTypeClientDto {
   @IsNotEmpty({ message: 'El id es obligatorio' })
   @IsInt({ message: 'El id debe ser entero' })
   id!: number;
@@ -9,5 +10,11 @@ export class PostAllTypeClientDto {
   @IsNotEmpty({ message: 'El nombre es obligatorio' })
   @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/, { message: 'El nombre solo debe contener letras y espacios' })
   nombre!: string;
-  
+}
+export class PostAllTypeClientArrayDto {
+  @IsArray({ message: 'El cuerpo debe ser un array de clientes' })
+  @ValidateNested({ each: true })
+  @Type(() => PostAllTypeClientDto)
+  @IsNotEmpty({ message: 'El array de clientes no puede estar vacío' })
+  typeClientes!: PostAllTypeClientDto[];
 }
--- src/modules/type_client/type_client.controller.ts
+++ src/modules/type_client/type_client.controller.ts
@@ -15,7 +15,7 @@ import { TypeClientServices } from './type_client.service';
 import { JwtAuthGuard } from '../../guards/jwt-auth.guard';
 import { RolesGuard } from '../../guards/role-check.guard';
 
-import { PostAllTypeClientDto } from './dto/post-AllTypeClient.dto';
+import { PostAllTypeClientArrayDto  } from './dto/post-AllTypeClient.dto';
 
 import { AuthRequest } from '../../types';
 
@@ -27,11 +27,9 @@ constructor(private readonly typeClientServices: TypeClientServices) {}
   /** ✅ Obtener todas las facturas (Solo admin) */
   @UseGuards(JwtAuthGuard, new RolesGuard([1]))
   @Post('admin/post-all-typeClient')
-  async submitAllTypeClient(@Req() request: AuthRequest,@Body() typeClientArray: PostAllTypeClientDto | PostAllTypeClientDto[]) {
-  // Asegurar que siempre sea un array
-  const clients = Array.isArray(typeClientArray) ? typeClientArray : [typeClientArray];
+  async submitAllTypeClient(@Req() request: AuthRequest,@Body() typeClientArray: PostAllTypeClientArrayDto ) {    
   
-  return await this.typeClientServices.submitAllTypeClient(request, clients);
+  return await this.typeClientServices.submitAllTypeClient(request, typeClientArray.typeClientes);
   }
 
 }
