51ab5cc3039b1de07d27905594b8f54ae7fe82e3
jpdzsoftware <jeremyivanpedraza@gmail.com>
2025-03-10 15:57:50 -0500

    actualización de auth code

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -83,6 +83,7 @@ export class AuthService {
           name: complementaryDataUser.name,
           lastname: complementaryDataUser.lastname,
           phone: complementaryDataUser.phone,
+          auth_code:data.auth_code,
           mobile: complementaryDataUser.mobile,
           created_at: complementaryDataUser.created_at,
           updated_at: complementaryDataUser.updated_at,
